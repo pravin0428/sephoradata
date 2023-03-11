a3ca4066b39cab96ba3b47e92eee0c456ec631c0
pravin0428 <mohite46199@gmail.com>
2023-03-11 21:44:58 +0530

    added userroute

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+// const authrouter = jsonServer.router("auth.json")
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
 server.use(middlewares);
 server.use(router);
+// server.use(authrouter)
 
 server.listen(port);
